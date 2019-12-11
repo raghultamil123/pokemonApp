2076ad48c68ed2ce97f120692423d68c55af9632
raghul tamilaiah <raghulkumar31960@gmail.com>
2019-12-11 22:28:08 +0530

    commit changes for notification loader

--- app/src/main/java/com/example/pokemonapp/Loader/PokemonLoader.java
+++ app/src/main/java/com/example/pokemonapp/Loader/PokemonLoader.java
@@ -1,5 +1,6 @@
 package com.example.pokemonapp.Loader;
 
+import android.app.Activity;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
@@ -8,22 +9,34 @@ import androidx.loader.content.AsyncTaskLoader;
 
 import com.example.pokemonapp.Helper.HelperClass;
 import com.example.pokemonapp.DTO.Pokemon;
+import com.example.pokemonapp.PokemonInfo;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonLoader extends AsyncTaskLoader<List<Pokemon>> {
-
+   public static WeakReference<PokemonInfo> mActivity;
     private String url="https://pokeapi.co/api/v2/pokemon/1/";
     public PokemonLoader(@NonNull Context context) {
+
         super(context);
+        Activity activity = (PokemonInfo)context;
+
+
     }
+    public PokemonLoader(PokemonInfo activity){
+        super(activity);
+        mActivity = new WeakReference<PokemonInfo>( activity);
+
+    }
+
 
     @Nullable
     @Override
     public List<Pokemon> loadInBackground() {
         List<Pokemon> pokemons = new ArrayList<>();
-        List<String> pokemonInfoUrl = new ArrayList<>();
+        final List<String> pokemonInfoUrl = new ArrayList<>();
         pokemonInfoUrl.add("https://pokeapi.co/api/v2/pokemon/1/");
         pokemonInfoUrl.add("https://pokeapi.co/api/v2/pokemon/4/");
         pokemonInfoUrl.add("https://pokeapi.co/api/v2/pokemon/7/");
@@ -35,10 +48,30 @@ public class PokemonLoader extends AsyncTaskLoader<List<Pokemon>> {
         pokemonInfoUrl.add("https://pokeapi.co/api/v2/pokemon/35");
         pokemonInfoUrl.add("https://pokeapi.co/api/v2/pokemon/42");
         pokemonInfoUrl.add("https://pokeapi.co/api/v2/pokemon/48");
+
+       int j = 0;
         for(String url:pokemonInfoUrl){
+            j++;
+            final int i = j;
       Pokemon pokemon= HelperClass.fetchFromUrl(url);
+            if(mActivity.get()!=null)
+            {
+                mActivity.get().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Ragn");
+                        double per = Double.valueOf(i)/Double.valueOf(pokemonInfoUrl.size())*100;
+                    mActivity.get().notification.setContentText(""+(Double.valueOf(Math.floor(per)).intValue())+"%").setProgress(pokemonInfoUrl.size(),i,false);
+                        mActivity.get(). notificationManager.notify(1, mActivity.get().notification.build());
+                    }
+                });
+
+            }
       pokemons.add(pokemon);
         }
+
+       mActivity.get().notification.setContentText("Records Taken  "+pokemonInfoUrl.size()).setProgress(0,0,false);
+        mActivity.get(). notificationManager.notify(1, mActivity.get().notification.build());
 
       return pokemons;
 
--- app/src/main/java/com/example/pokemonapp/PokemonInfo.java
+++ app/src/main/java/com/example/pokemonapp/PokemonInfo.java
@@ -26,6 +26,7 @@ import com.example.pokemonapp.Adapter.PokemonInfoAdapter;
 import com.example.pokemonapp.DTO.Pokemon;
 import com.example.pokemonapp.Loader.PokemonLoader;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,15 +35,18 @@ public class PokemonInfo extends AppCompatActivity implements LoaderManager.Load
     TextView textView;
     PokemonInfoAdapter pokemonInfoAdapter;
     List<Pokemon> pokemons;
-
+   public NotificationManager notificationManager;
+  public  NotificationChannel notificationChannel;
+  public  NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "netowork");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_info);
-
+        PokemonLoader.mActivity = new WeakReference<PokemonInfo>(this);
         pokemons = new ArrayList<>();
         pokemonInfoAdapter = new PokemonInfoAdapter(this,pokemons);
         setTitle("PokemonInfo");
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "netowork");
         ListView listView = findViewById(R.id.resultList);
         listView.setAdapter(pokemonInfoAdapter);
         View emptyView = findViewById(R.id.empty_view);
@@ -76,24 +80,28 @@ public class PokemonInfo extends AppCompatActivity implements LoaderManager.Load
 
     private void createNotification(String connectionText) {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel("netowork" ,"internet_connection", NotificationManager.IMPORTANCE_HIGH);
+             notificationChannel = new NotificationChannel("netowork" ,"internet_connection", NotificationManager.IMPORTANCE_HIGH);
             notificationChannel.enableLights(true);
             notificationChannel.enableVibration(true);
             notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
            Intent intent =new Intent(this,PokemonInfo.class);
-           intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+           //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+          //  PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+             notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.createNotificationChannel(notificationChannel);
-            NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "netowork")
+             notification = new NotificationCompat.Builder(this, "netowork")
                     .setContentTitle("Connection")
                     .setContentText(connectionText)
-                    .setContentIntent(pendingIntent)
+                   // .setContentIntent(pendingIntent)
                     .setAutoCancel(true)
                     .setSmallIcon(R.mipmap.ic_launcher_round);
-
-
+       /*     int progressMax = 100;
+            int progressMin = 0;
+            notification.setProgress(progressMax,progressMin,false);*/
             notificationManager.notify(1, notification.build());
+           // notification.setContentText("completed").setProgress(0,0,false);
+
+
 
         }
     }
@@ -101,11 +109,14 @@ public class PokemonInfo extends AppCompatActivity implements LoaderManager.Load
     @NonNull
     @Override
     public Loader<List<Pokemon>> onCreateLoader(int id, @Nullable Bundle args) {
+        createNotification("Fetching records from server Please Wait...");
         return new PokemonLoader(this);
     }
 
     @Override
     public void onLoadFinished(@NonNull Loader<List<Pokemon>> loader, List<Pokemon> data) {
+        notification.setContentText("Completed Thanks for your cooperation").setProgress(0,0,false);
+        notificationManager.notify(1, notification.build());
      pokemons.addAll(data);
      pokemonInfoAdapter.clear();
 
@@ -117,4 +128,5 @@ public class PokemonInfo extends AppCompatActivity implements LoaderManager.Load
     public void onLoaderReset(@NonNull Loader<List<Pokemon>> loader) {
 
     }
+
 }
